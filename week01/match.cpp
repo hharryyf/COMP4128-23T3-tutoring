51fe3898279258e2a64c0f621f03f471784b9bda
hharryyf <harryheyifan99@gmail.com>
2023-09-13 15:40:07 +1000

    update

--- week01/match.cpp
+++ week01/match.cpp
@@ -17,8 +17,31 @@ using namespace std;
 int x[MAX_SIZE];
 int N, Z;
 
+bool check(int K) {
+    for (int i = 1; i <= K; ++i) {
+        if (i + N - K > N) return false;
+        if (x[i+N-K] - x[i] < Z) return false;
+    }
+
+    return true;
+}
 
 int main() {
-    
+    int i;
+    scanf("%d%d", &N, &Z);
+    for (i = 1; i <= N; ++i) scanf("%d", &x[i]);
+    sort(x + 1, x + 1 + N);
+    int low = 1, high = N / 2, ans = 0;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (check(mid)) {
+            low = mid + 1;
+            ans = mid;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    printf("%d\n", ans);
     return 0;
 }
--- week01/word.cpp
+++ week01/word.cpp
@@ -18,7 +18,84 @@ AABCDEFGHIJKLMNOPQRSTUVW??M
 */
 
 
+char s[MAX_SIZE];
+// tol is the total number of letters in the window
+// dist is the total number of unique letters in the window
+// AAABB dist = 2
+int tol = 0, dist = 0;
+// cnt[i] is the total number of occurences of 'A' + i in the window
+int cnt[27];
+
+// add a letter to our window
+void add(char ch) {
+    if (ch == '?') return;
+    cnt[ch - 'A']++;
+    tol++;
+    // A 0->1 there's a new letter 'A'
+    if (cnt[ch - 'A'] == 1) {
+        dist++;
+    }
+}
+
+// delete a letter from our window
+void del(char ch) {
+    if (ch == '?') return;
+    cnt[ch - 'A']--;
+    tol--;
+    // A has a count 1, when the count is 0 1->0, A is no longer in the window
+    // the number of unique letters no longer contains 'A'
+    if (cnt[ch - 'A'] == 0) {
+        dist--;
+    }
+}
+
+// judge whether the window contains only distinct letters
+bool query() {
+    return dist == tol;
+}
+
 int main() {
-    
+    scanf("%s", s);
+    int i, j, N = strlen(s), ok = false;
+    for (i = 0; i < N; ++i) {
+        add(s[i]);
+        // window range [i-25, i]
+        if (i >= 25 && query()) {
+            ok = true;
+            vector<char> rem;
+            // complete the string
+            for (j = 0; j < 26; ++j) {
+                if (cnt[j] == 0) {
+                    rem.push_back('A' + j);
+                    //printf("%c\n", 'A' + j);
+                }
+            }
+
+            for (j = i - 25; j <= i; ++j) {
+                if (s[j] == '?') {
+                    
+                    s[j] = rem.back();
+                    rem.pop_back();
+                    // printf("%s\n", s);
+                }
+            }
+            break;
+        }
+
+        if (i >= 25) {
+            del(s[i-25]);
+        }
+    }
+
+    if (ok) {
+        for (i = 0; i < N; ++i) {
+            if (s[i] == '?') {
+                s[i] = 'A';
+            }
+        }
+        printf("%s\n", s);
+    } else {
+        printf("-1\n");
+    }
     return 0;
 }

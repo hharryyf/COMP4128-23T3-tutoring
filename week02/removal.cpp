5bd5627c254adca8d60791b5f2af2d23560e1c5a
hharryyf <harryheyifan99@gmail.com>
2023-09-18 16:41:17 +1000

    update

---
+++ week02/removal.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define MAX_SIZE 511
+using namespace std;
+
+char s[MAX_SIZE];
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while (T-- > 0) {
+        scanf("%s", s + 1);
+        
+    }
+    return 0;
+}

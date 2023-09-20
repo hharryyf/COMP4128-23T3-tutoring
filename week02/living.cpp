68dc503d110a7b317b93a063e6bd5743ba313a7e
hharryyf <harryheyifan99@gmail.com>
2023-09-20 15:23:59 +1000

    update

--- week02/living.cpp
+++ week02/living.cpp
@@ -6,7 +6,53 @@ using namespace std;
 int pre[3001][3001];
 
 int rectangle(int R,int C,int H,int W,int Q[3001][3001]) {
-    
+    int i, j;
+    for (i = R; i >= 1; --i) {
+        for (j = C; j >= 1; --j) {
+            Q[i][j] = Q[i-1][j-1];
+        }
+    }
+
+    auto check = [&](int K) -> bool {
+        for (i = 1; i <= R; ++i) {
+            for (j = 1; j <= C; ++j) {
+                if (Q[i][j] <= K) {
+                    pre[i][j] = 1;
+                } else {
+                    pre[i][j] = -1;
+                }
+            }
+        }
+
+        for (i = 1; i <= R; ++i) {
+            for (j = 1; j <= C; ++j) {
+                pre[i][j] += pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
+            }
+        }
+
+        for (i = H; i <= R; ++i) {
+            for (j = W; j <= C; ++j) {
+                if (pre[i][j] + pre[i-H][j-W] - pre[i-H][j] - pre[i][j-W] > 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    };
+
+    int ans = R * C, low = 1, high = R * C;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (check(mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
 }
 
 int a[3001][3001];
--- week02/removal.cpp
+++ week02/removal.cpp
@@ -4,12 +4,54 @@ using namespace std;
 
 char s[MAX_SIZE];
 
+
+/*
+
+    5
+    01111001
+    0000
+    111111
+    101010101
+    011011110111
+
+    4
+    0
+    6
+    3
+    6
+*/
+
 int main() {
-    int T;
+    int T, N;
     scanf("%d", &T);
     while (T-- > 0) {
+        int cnt = 0;
         scanf("%s", s + 1);
-        
+        N = strlen(s + 1);
+        vector<int> group;
+        for (int i = 1; i <= N; ++i) {
+            if (s[i] == '1') {
+                cnt++;
+            } else {
+                if (cnt > 0) {
+                    group.push_back(cnt); 
+                }
+                cnt = 0;
+            }
+        }
+
+        if (cnt > 0) {
+            group.push_back(cnt);
+        }
+
+        sort(group.begin(), group.end(), greater<int>());
+
+        int ans = 0;
+        for (int i = 0 ; i < (int) group.size(); i = i + 2) {
+            ans += group[i];
+        }
+
+        printf("%d\n", ans);
     }
     return 0;
 }

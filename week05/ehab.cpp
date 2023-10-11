4386d5e2be77b3ba2939263cfb9317a06a21d625
hharryyf <harryheyifan99@gmail.com>
2023-10-11 16:19:52 +1100

    update

--- week05/ehab.cpp
+++ week05/ehab.cpp
@@ -3,11 +3,99 @@
 using namespace std;
 
 vector<int> g[MAX_SIZE];
+// depth[v] is the depth of vertex v in the dfs tree of the graph
+// f[v] is the parent node of v in the dfs tree
 int depth[MAX_SIZE], f[MAX_SIZE];
 int N, M, K;
 
+void dfs(int v, int pre) {
+    depth[v] = depth[pre] + 1;
+    f[v] = pre;
+    // (v, back_vertex) is a backedge in the dfs
+    // we want to find the back_vertex connected to v that has the greatest depth
+    int back_vertex = -1;
+
+    for (auto nv : g[v]) {
+        if (nv != pre) {
+            // some vertex is connected to v and has been visited 
+            if (depth[nv] > 0) {
+                // nv is a back_vertex, (v, nv) is a backedge
+                if (back_vertex == -1 || depth[back_vertex] < depth[nv]) {
+                    back_vertex = nv;
+                }
+            }
+        }
+    }
+
+    if (back_vertex != -1) {
+        // we know we have a cycle without edges crossing in the middle
+        int curr = v;
+        vector<int> ret;
+        while (curr != back_vertex) {
+            ret.push_back(curr);
+            curr = f[curr];
+        }
+
+        ret.push_back(curr);
+
+        if ((int) ret.size() > K) {
+            printf("1\n");
+            for (int i = 0, j = 0 ; i < (int) ret.size() && j < (K + 1) / 2; i = i + 2, ++j) {
+                printf("%d ", ret[i]);
+            }
+            printf("\n");
+        } else {
+            printf("2\n%d\n", (int) ret.size());
+            for (auto v : ret) {
+                printf("%d ", v);
+            }
+
+            printf("\n");
+            
+        }
+
+        exit(0);
+    }
+
+    for (auto nv : g[v]) {
+        if (depth[nv] == 0) {
+            dfs(nv, v);
+        }
+    }
+}
 
 int main() {
-    
+    int i;
+    // N vertices, M edges, K is the K in the problem statement
+    scanf("%d%d%d", &N, &M, &K);    
+    for (i = 1; i <= M; ++i) {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    dfs(1, 0);
+
+    vector<int> odd, even;
+
+    for (i = 1; i <= N; ++i) {
+        if (depth[i] % 2 == 1) {
+            odd.push_back(i);
+        } else {
+            even.push_back(i);
+        }
+    }
+
+    if (odd.size() < even.size()) {
+        swap(odd, even);
+    }
+
+    printf("3\n");
+    for (int i = 0, j = 0 ; i < (int) odd.size() && j < (K + 1) / 2; ++i, ++j) {
+        printf("%d ", odd[i]);
+    }
+    printf("\n");
+
     return 0;
 }
--- week05/grid.cpp
+++ week05/grid.cpp
@@ -4,7 +4,9 @@ using namespace std;
 typedef long long ll;
 
 struct edge {
+    // u, v means the end points of the edge
     int u, v;
+    // c means the weight of the edge
     ll c;
     bool operator < (edge &other) const {
         return c < other.c;
@@ -13,11 +15,88 @@ struct edge {
 
 vector<edge> edg;
 
+// line 19 - 34, you can copy directly, Union-find data structure
+// father/root of point i
+int f[MAX_SIZE];
+
+int find(int x) {
+    if (f[x] == x) return x;
+    return f[x] = find(f[x]);
+}
+
+void unionset(int x, int y) {
+    int fx = find(x), fy = find(y);
+    f[fx] = fy;
+}
+
+void init() {
+    for (int i = 0 ; i < MAX_SIZE; ++i) f[i] = i;
+}
+
+
+
 int N;
 
 int x[MAX_SIZE], y[MAX_SIZE], c[MAX_SIZE], k[MAX_SIZE];
 
+
+
 int main() {
-    
+    init();
+    scanf("%d", &N);
+    for (int i = 1; i <= N; ++i) {
+        scanf("%d%d", &x[i], &y[i]);
+    }   
+
+    for (int i = 1; i <= N; ++i) scanf("%d", &c[i]);
+    for (int i = 1; i <= N; ++i) scanf("%d", &k[i]);
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = i + 1; j <= N; ++j) {
+            edg.push_back({i, j, 1ll * (k[i] + k[j]) * (abs(x[i] - x[j]) + abs(y[i] - y[j]))});
+        }
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        edg.push_back({0, i, c[i]});
+    }
+
+    sort(edg.begin(), edg.end());
+
+    // stores the connection between cities
+    vector<pair<int, int>> conn;
+    // stores the cities directly connected to power
+    vector<int> pw;
+    // ans is the minimum cost
+    ll ans = 0;
+
+    for (auto &e : edg) {
+        // means e.u and e.v belong to different connected component
+        // adding this edge will not create a cycle
+        if (find(e.u) != find(e.v)) {
+            ans += e.c;
+            unionset(e.u, e.v);
+            if (e.u == 0 || e.v == 0) {
+                pw.push_back(e.u + e.v);
+            } else {
+                conn.emplace_back(e.u, e.v);
+            }
+        }
+    }
+
+    printf("%lld\n", ans);
+
+    printf("%d\n", (int) pw.size());
+
+    for (auto &p : pw) {
+        printf("%d ", p);
+    }
+
+    printf("\n%d\n", (int) conn.size());
+
+    for (auto &p : conn) {
+        printf("%d %d\n", p.first, p.second);
+    }
+
     return 0;
 }

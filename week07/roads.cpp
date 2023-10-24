552246a3f30587e0a4ef25d470f655f3c59b07d5
hharryyf <harryheyifan99@gmail.com>
2023-10-24 22:34:45 +1100

    update

---
+++ week07/roads.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define MAX_SIZE 311
+using namespace std;
+typedef long long ll;
+
+ll dp[MAX_SIZE][MAX_SIZE];
+int N, K;
+
+int main() {
+    
+    return 0;
+}
---
+++ week07/weights.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define MAX_SIZE 200011
+using namespace std;
+typedef long long ll;
+
+int N, M, A, B, C;
+
+vector<int> g[MAX_SIZE];
+ll p[MAX_SIZE];
+
+
+int main() {
+    int T, i;
+    scanf("%d", &T);
+    while (T-- > 0) {
+        scanf("%d%d%d%d%d", &N, &M, &A, &B, &C);
+        
+    }
+    return 0;
+}

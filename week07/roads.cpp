1aad1d41c90dec8597614926aca75d288206146c
hharryyf <harryheyifan99@gmail.com>
2023-10-25 16:41:30 +1100

    update

--- week07/roads.cpp
+++ week07/roads.cpp
@@ -3,10 +3,48 @@
 using namespace std;
 typedef long long ll;
 
-ll dp[MAX_SIZE][MAX_SIZE];
+ll dist[MAX_SIZE][MAX_SIZE];
 int N, K;
 
 int main() {
-    
+    int i, j, k;
+    scanf("%d", &N);
+    for (i = 1; i <= N; ++i) {
+        for (j = 1; j <= N; ++j) {
+            scanf("%lld", &dist[i][j]);
+        }
+    }
+
+    // dist[i][j] stores the shortest distance between i and j
+    // Floyd algorithm
+    for (k = 1; k <= N; ++k) {
+        for (i = 1; i <= N; ++i) {
+            for (j = 1; j <= N; ++j) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    scanf("%d", &K);
+    while (K-- > 0) {
+        int a, b;
+        ll c;
+        scanf("%d%d%lld", &a, &b, &c);
+        // consider how e(a, b, c) affects the shortest distance between (i, j)
+        for (i = 1; i <= N; ++i) {
+            for (j = 1; j <= N; ++j) {
+                dist[i][j] = min(dist[i][j], min(dist[i][a] + dist[b][j] + c, dist[i][b] + dist[a][j] + c));
+            }
+        }
+
+        ll ans = 0;
+        for (i = 1; i <= N; ++i) {
+            for (j = i + 1; j <= N; ++j) {
+                ans = ans + dist[i][j];
+            }
+        }
+
+        printf("%lld\n", ans);
+    }
     return 0;
 }
--- week07/weights.cpp
+++ week07/weights.cpp
@@ -8,13 +8,76 @@ int N, M, A, B, C;
 vector<int> g[MAX_SIZE];
 ll p[MAX_SIZE];
 
+// dist[0][v] = the shortest distance between A and v
+// dist[1][v] = the shortest distance between B and v
+// dist[2][v] = the shortest distance between C and v
+ll dist[3][MAX_SIZE];
+int visited[MAX_SIZE];
+
+void BFS(int src, int dim) {
+    int i;
+    for (i = 1; i <= N; ++i) {
+        dist[dim][i] = 1000000000000ll;
+        visited[i] = 0;
+    }
+
+    queue<int> q;
+
+    q.push(src);
+    dist[dim][src] = 0;
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        if (visited[v]) continue;
+        visited[v] = 1;
+        for (auto nv : g[v]) {
+            if (!visited[nv] && dist[dim][nv] > dist[dim][v] + 1) {
+                dist[dim][nv] = dist[dim][v] + 1;
+                q.push(nv);
+            }
+        }
+    }
+}
 
 int main() {
     int T, i;
     scanf("%d", &T);
     while (T-- > 0) {
         scanf("%d%d%d%d%d", &N, &M, &A, &B, &C);
-        
+        for (i = 1; i <= M; ++i) scanf("%lld", &p[i]);
+        sort(p + 1, p + 1 + M);
+
+        for (i = 1; i <= N; ++i) g[i].clear();
+
+        for (i = 1; i <= M; ++i) {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+
+        BFS(A, 0);
+        BFS(B, 1);
+        BFS(C, 2);
+
+        for (i = 1; i <= M; ++i) {
+            p[i] = p[i-1] + p[i];
+        }
+
+        ll ans = 1e17;
+        for (i = 1; i <= N; ++i) {
+            // we iterate through the vertex i
+            // i is the vertex v on the slide
+            // we go from a -> v -> b -> v -> c
+            // smallest dist(b, i) many edges from p as k2
+            // smallest dist(a, i) + dist(b, i) + dist(c, i) many edges as k1/k2/k3
+            if (dist[0][i] + dist[1][i] + dist[2][i] <= M) {
+                ans = min(ans, p[dist[1][i]] + p[dist[0][i] + dist[1][i] + dist[2][i]]);
+            }
+        }
+
+        printf("%lld\n", ans);
     }
     return 0;
 }

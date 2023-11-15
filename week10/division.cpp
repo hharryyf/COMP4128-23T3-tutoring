d3aaee8bc3300d86302af340480258670baaf138
hharryyf <harryheyifan99@gmail.com>
2023-11-15 16:27:21 +1100

    update

--- week10/division.cpp
+++ week10/division.cpp
@@ -8,7 +8,37 @@ int main() {
     while (T-- > 0) {
         ll A, B;
         scanf("%lld%lld", &A, &B);
-        
+        vector<pair<ll, int>> prime;
+        ll tmp = B;
+        for (ll i = 2; i * i <= B; ++i) {
+            int cnt = 0;
+            while (tmp % i == 0) {
+                cnt++;
+                tmp /= i;
+            }
+
+            if (cnt > 0) prime.emplace_back(i, cnt);
+        }
+
+        if (tmp > 1) prime.emplace_back(tmp, 1);
+
+        if (A % B == 0) {
+            ll ans = 0;
+            // M * p1 ^ (d1 - 1) * p2 ^ c2 * p3 ^ c3 * ...
+            for (auto &p : prime) {
+                tmp = A;
+                while (tmp % p.first == 0) tmp /= p.first;
+                for (int i = 1; i < p.second; ++i) {
+                    tmp = tmp * p.first;
+                }
+
+                ans = max(ans, tmp);
+            }
+
+            printf("%lld\n", ans);
+        } else {
+            printf("%lld\n", A);
+        }
     }
     return 0;
 }
--- week10/race.cpp
+++ week10/race.cpp
@@ -7,6 +7,29 @@ ld dp[MAX_SIZE][MAX_SIZE][2];
 
 // Answer: 0.83648556
 
+ld f(int i, int j, int turn) {
+    if (i >= 100) return 1 - turn;
+    if (j >= 100) return turn;
+    if (dp[i][j][turn] != -1) return dp[i][j][turn];
+    if (turn == 0) {
+        return dp[i][j][turn] = (0.5 * (1 - f(i + 1, j, 1)) + 0.5 * (1 - f(i, j, 1)));
+    } else {
+        ld ans = 0;
+        for (int t = 1; t <= 8; ++t) {
+            ld score = f(i, j + (1 << (t - 1)), 0), p = 1.0 / (1 << t);
+            ans = max(ans, (p * 2) / (1.0 + p) * (1 - score) + 1.0 * (1 - p) / (1 + p) * f(i + 1, j, 1));
+        }
+
+        return dp[i][j][turn] = ans;
+    }
+}
+
 int main() {
+    for (int i = 0 ; i < MAX_SIZE; ++i) {
+        for (int j = 0 ; j < MAX_SIZE; ++j) {
+            dp[i][j][0] = dp[i][j][1] = -1;
+        }
+    }
+    printf("%.8Lf\n", 1 - f(0, 0, 0));
     return 0;
 }
